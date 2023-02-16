4968e9ee15d44a63bd185e21c4c4e5a516cff8b9
yoobikkk <yoobikkk@naver.com>
2023-02-16 21:21:13 +0900

    jsparser

--- commit.bat
+++ commit.bat
@@ -1,10 +1,13 @@
 set path=C:\Program Files\Git\bin
 
-git remote add origin https://github.com/janghunduck/jsparser.git
+
 git add .
 git commit -m "jsparser"
 git status
 git config --list
+
+git remote add origin https://github.com/janghunduck/jsparser.git
+git pull origin master
 git push origin master
 
 
