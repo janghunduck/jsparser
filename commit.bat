df9f8e64e3d63a7a2148b046540e06e611dc8db4
yoobikkk <yoobikkk@naver.com>
2023-03-15 01:04:16 +0900

    new

--- commit.bat
+++ commit.bat
@@ -14,9 +14,7 @@ git config --list
 
 git remote add origin https://github.com/janghunduck/jsparser.git
 rem master is this(노예) so, change main.
-rem git pull origin master
+git pull origin master
 git push origin master
-
-
 
 pause
